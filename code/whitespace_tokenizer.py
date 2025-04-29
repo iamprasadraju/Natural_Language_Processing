59aa263919f8b5cd214c47bd5906de85f78b9040
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-04-29 12:39:11 +0530

    added an example

--- code/whitespace_tokenizer.py
+++ code/whitespace_tokenizer.py
@@ -1,5 +1,11 @@
-#Whitespace Tokenizer
+# Whitespace Tokenizer
 
 def WhitesapceTokenizer(str):
     words = str.split(" ")
     return words
+
+
+sentence = "I can't believe it's 3.14!"
+tokens = WhitesapceTokenizer(sentence)
+
+print(tokens)
